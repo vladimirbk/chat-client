000fb0914d68643aee2bee4df821416177a050d5
badashkhanov <vladimir.badashkhanov@avast.com>
2019-12-18 17:25:01 +0100

    init

---
+++ src/main/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import gui.ChatWindow;
+
+/**
+ *
+ * @author badashkhanov
+ */
+public class Main {
+    
+    private static void main(String[] args){
+        new ChatWindow().setVisible(true);
+    }
+    
+}
